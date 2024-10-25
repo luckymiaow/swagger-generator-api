64e82581d4899e0d2df6f0d83c1b756e60ea3f40
luckymiaow <1624963361@qq.com>
2024-10-25 23:42:45 +0800

    修复递归死循环

--- lib/code-gen/type-builder.d.ts
+++ lib/code-gen/type-builder.d.ts
@@ -1,6 +1,6 @@
 import type { OpenAPI3Reference, OpenAPI3SchemaObject, OpenAPI3Schemas } from '../schema';
 import type { DotNetTypes, IDotnetType, IDotnetTypeRef } from './types';
 export declare function buildTypeRef(schema: OpenAPI3SchemaObject, types: DotNetTypes, schemas?: OpenAPI3Schemas): IDotnetTypeRef;
-export declare function buildType(obj: OpenAPI3SchemaObject | OpenAPI3Reference, types: DotNetTypes, schemas?: OpenAPI3Schemas): IDotnetType;
-export declare function findOrBuildType(typeRef: string, obj: OpenAPI3SchemaObject | OpenAPI3Reference, types: DotNetTypes, schemas?: OpenAPI3Schemas): IDotnetType;
+export declare function buildType(obj: OpenAPI3SchemaObject | OpenAPI3Reference, types: DotNetTypes, schemas?: OpenAPI3Schemas, visited?: Set<string>): IDotnetType;
+export declare function findOrBuildType(typeRef: string, obj: OpenAPI3SchemaObject | OpenAPI3Reference, types: DotNetTypes, schemas?: OpenAPI3Schemas, visited?: Set<string>): IDotnetType;
 export declare function buildTypes(schemas: OpenAPI3Schemas): DotNetTypes;
